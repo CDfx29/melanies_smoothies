22502003eeeb7f792c82404b55217b120c89cb89
CDfx29 <167036910+CDfx29@users.noreply.github.com>
2024-04-15 16:30:44 +1200

    Update streamlit_app.py
    st

--- streamlit_app.py
+++ streamlit_app.py
@@ -23,8 +23,8 @@ my_dataframe = session.table("smoothies.public.fruit_options") . select(col('FRU
 
 #Convert the snowpark Dataframe to a Panda dataframe
 pd_df=my_dataframe.to_pandas()
-st.dataframe(pd_df)
-st.stop()
+#st.dataframe(pd_df)
+#st.stop()
 
 
 ingredients_list=st.multiselect(
