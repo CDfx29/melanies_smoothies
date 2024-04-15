5a062d51c3d0fa31036d8180edf9030296774d0d
CDfx29 <167036910+CDfx29@users.noreply.github.com>
2024-04-15 16:13:24 +1200

    Update streamlit_app.py
    pndimp+

--- streamlit_app.py
+++ streamlit_app.py
@@ -18,8 +18,14 @@ cnx=st.connection("snowflake")
 session=cnx.session()
 #session = get_active_session()
 my_dataframe = session.table("smoothies.public.fruit_options") . select(col('FRUIT_NAME'),col('SEARCH_ON'))
-st.dataframe(data=my_dataframe, use_container_width=True)
-st.Stop()
+#st.dataframe(data=my_dataframe, use_container_width=True)
+#st.Stop()
+
+#Convert the snowpark Dataframe to a Panda dataframe
+pd_df=my_dataframe.to_pandas()
+st.dataframe(pd_df)
+st.stop()
+
 
 ingredients_list=st.multiselect(
     'choose up to 5 ingredients:'
